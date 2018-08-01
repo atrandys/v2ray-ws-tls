f3437bfa60ce91d9b955207fbb37cfee6d631af9
yobabyshark <37266675+yobabyshark@users.noreply.github.com>
2018-08-01 21:02:55 +0800

    Update config_v2ray.sh

--- config_v2ray.sh
+++ config_v2ray.sh
@@ -14,7 +14,6 @@ rand(){
     echo $(($num%$max+$min))  
 }
 yum install -y wget
-
 #获取本机外网ip
 serverip=$(curl icanhazip.com)
 
@@ -25,12 +24,13 @@ cd /etc/v2ray/
 rm -f config.json
 
 #下载kcp+tcp配置文件，kcp（srtp混淆），tcp（http混淆）
-wget https://raw.githubusercontent.com/yobabyshark/proV/master/config.json
+wget https://raw.githubusercontent.com/yobabyshark/prov-ws/master/config.json
 
 #生成并替换uuid，kcp、tcp各一个
 kcpuuid=$(cat /proc/sys/kernel/random/uuid)
-tcpuuid=$(cat /proc/sys/kernel/random/uuid)
-sed -i "s/aaaa/$kcpuuid/;s/bbbb/$tcpuuid/;" config.json
+sed -i "s/aaaa/$kcpuuid/;" config.json
+newpath="/ray"$(cat /dev/urandom | head -1 | md5sum | head -c 8)
+sed -i "s/mypath/$newpath/;" config.json
 
 #生成并修改端口
 port=$(rand 10000 30000)
@@ -48,20 +48,10 @@ cat > /etc/v2ray/myconfig.json<<-EOF
 uuid：${kcpuuid}
 额外id：64
 加密方式：aes-128-gcm
-传输协议：kcp
-别名：mykcp
-伪装类型：srtp
-
-===========TCP配置=============
-地址：${serverip}
-端口：${port}
-uuid：${tcpuuid}
-额外id：64
-加密方式：aes-128-gcm
-传输协议：tcp
-别名：mytcp
-伪装类型：http
-伪装域名：bing.com
+传输协议：ws
+别名：myws
+路径：${newpath}
+底层传输：tls
 }
 EOF
 
@@ -77,21 +67,12 @@ echo "端口：${port}"
 echo "uuid：${kcpuuid}"
 echo "额外id：64"
 echo "加密方式：aes-128-gcm"
-echo "传输协议：kcp"
-echo "别名：mykcp"
-echo "伪装类型：srtp"
+echo "传输协议：ws"
+echo "别名：myws"
+echo "路径：${newpath}"
+echo "底层传输：tls"
 echo 
-echo "===========TCP配置============="
-echo "地址：${serverip}"
-echo "端口：${port}"
-echo "uuid：${tcpuuid}"
-echo "额外id：64"
-echo "加密方式：aes-128-gcm"
-echo "传输协议：tcp"
-echo "别名：mytcp"
-echo "伪装类型：http"
-echo "伪装域名：bing.com"
-echo
+
 
 
 
