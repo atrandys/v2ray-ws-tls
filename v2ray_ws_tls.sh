37247cc91ac68c832ca60c58f0fbf9360547308e
atrandys <37266675+atrandys@users.noreply.github.com>
2019-02-23 00:29:12 +0800

    Update v2ray_ws_tls.sh

--- v2ray_ws_tls.sh
+++ v2ray_ws_tls.sh
@@ -52,7 +52,7 @@ events {
     worker_connections  1024;
 }
 http {
-    include       /etc/nginx/mime.types;
+    include       /etc/nginx/conf/mime.types;
     default_type  application/octet-stream;
     log_format  main  '\$remote_addr - \$remote_user [\$time_local] "\$request" '
                       '\$status \$body_bytes_sent "\$http_referer" '
