5b5f5d22adcbd723e33c0e2da496f213ce66a169
atrandys <37266675+atrandys@users.noreply.github.com>
2019-02-22 16:46:13 +0800

    Update v2ray_ws_tls.sh

--- v2ray_ws_tls.sh
+++ v2ray_ws_tls.sh
@@ -99,6 +99,12 @@ server {
     index index.php index.html;
     ssl_certificate /etc/nginx/ssl/fullchain.cer; 
     ssl_certificate_key /etc/nginx/ssl/$domain.key;
+    #TLS 版本控制
+    ssl_protocols   TLSv1 TLSv1.1 TLSv1.2 TLSv1.3;
+    ssl_ciphers     'TLS13-AES-256-GCM-SHA384:TLS13-CHACHA20-POLY1305-SHA256:TLS13-AES-128-GCM-SHA256:TLS13-AES-128-CCM-8-SHA256:TLS13-AES-128-CCM-SHA256:EECDH+CHACHA20:EECDH+CHACHA20-draft:EECDH+ECDSA+AES128:EECDH+aRSA+AES128:RSA+AES128:EECDH+ECDSA+AES256:EECDH+aRSA+AES256:RSA+AES256:EECDH+ECDSA+3DES:EECDH+aRSA+3DES:RSA+3DES:!MD5';
+
+    # 开启 1.3 0-RTT
+    ssl_early_data  on;
     ssl_stapling on;
     ssl_stapling_verify on;
     add_header Strict-Transport-Security "max-age=31536000";
