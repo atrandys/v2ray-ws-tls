77b9230a9e7ddf71e0d3c15e937656e49124039e
atrandys <37266675+atrandys@users.noreply.github.com>
2019-12-07 19:10:54 +0800

    Update v2ray_ws_tls_with_wp.sh

--- v2ray_ws_tls_with_wp.sh
+++ v2ray_ws_tls_with_wp.sh
@@ -387,7 +387,7 @@ install_wp(){
     sleep 1
     sed -i "s/database_name_here/wordpress_db/;s/username_here/root/;s/password_here/$mysqlpasswd/;" /usr/share/nginx/html/wp-config.php
     echo "define('FS_METHOD', "direct");" >> /usr/share/nginx/html/wp-config.php
-    chown -R nginx.root /usr/share/nginx/html/
+    chown -R nginx /usr/share/nginx/html/
     chmod -R 777 /usr/share/nginx/html/wp-content
     green "==========================================================="
     green " WordPress服务端配置已完成，请打开浏览器访问您的域名进行前台配置"
