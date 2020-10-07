93a4180496b9b6c23201e7a25543541f5c42a1d8
atrandys <37266675+atrandys@users.noreply.github.com>
2020-10-07 14:00:16 +0800

    Update vless_ws_tls.sh

--- vless_ws_tls.sh
+++ vless_ws_tls.sh
@@ -166,14 +166,6 @@ server {
     index index.php index.html;
     ssl_certificate /etc/nginx/ssl/fullchain.cer; 
     ssl_certificate_key /etc/nginx/ssl/private.key;
-    ssl_protocols   TLSv1.2 TLSv1.3;
-    ssl_ciphers     'TLS13-AES-256-GCM-SHA384:TLS13-CHACHA20-POLY1305-SHA256:TLS13-AES-128-GCM-SHA256:TLS13-AES-128-CCM-8-SHA256:TLS13-AES-128-CCM-SHA256:EECDH+CHACHA20:EECDH+CHACHA20-draft:EECDH+ECDSA+AES128:EECDH+aRSA+AES128:RSA+AES128:EECDH+ECDSA+AES256:EECDH+aRSA+AES256:RSA+AES256:EECDH+ECDSA+3DES:EECDH+aRSA+3DES:RSA+3DES:!MD5';
-    ssl_prefer_server_ciphers   on;
-    ssl_early_data  on;
-    ssl_stapling on;
-    ssl_stapling_verify on;
-    #add_header Strict-Transport-Security "max-age=31536000";
-    #access_log /var/log/nginx/access.log combined;
     location /$newpath {
         proxy_redirect off;
         proxy_pass http://127.0.0.1:11234; 
