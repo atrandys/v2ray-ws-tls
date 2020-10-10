e4bf71fa308872b197ee2b1a420daa562213abf0
atrandys <37266675+atrandys@users.noreply.github.com>
2020-10-10 11:24:09 +0800

    Update vless_tcp_xtls_wp.sh

--- vless_tcp_xtls_wp.sh
+++ vless_tcp_xtls_wp.sh
@@ -39,6 +39,164 @@ loggreen "== OS    : $RELEASE $VERSION"
 loggreen "== Kernel: $(uname -r)"
 loggreen "== User  : $(whoami)"
 sleep 2s
+
+install_wordpress(){
+    logcmd "yum install -y iptables-services"
+    systemctl start iptables
+    systemctl enable iptables
+    iptables -F
+    SSH_PORT=$(awk '$1=="Port" {print $2}' /etc/ssh/sshd_config)
+    if [ ! -n "$SSH_PORT" ]; then
+        iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
+    else
+        iptables -A INPUT -p tcp -m tcp --dport ${SSH_PORT} -j ACCEPT
+    fi
+    iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
+    iptables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT
+    iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+    iptables -A INPUT -i lo -j ACCEPT
+    iptables -P INPUT DROP
+    iptables -P FORWARD DROP
+    iptables -P OUTPUT ACCEPT
+    service iptables save
+    green "====================================================================="
+    green "安全起见，iptables仅开启ssh,http,https端口，如需开放其他端口请自行放行"
+    green "====================================================================="
+    sleep 1
+    yum -y install  wget
+    mkdir /usr/share/wordpresstemp
+    cd /usr/share/wordpresstemp/
+    wget https://cn.wordpress.org/latest-zh_CN.zip
+    if [ ! -f "/usr/share/wordpresstemp/latest-zh_CN.zip" ]; then
+        logred "从cn官网下载wordpress失败，尝试从github下载……"
+        logcmd "wget https://github.com/atrandys/wordpress/raw/master/latest-zh_CN.zip"
+    fi
+    if [ ! -f "/usr/share/wordpresstemp/latest-zh_CN.zip" ]; then
+        logred "从github下载wordpress也失败了，请尝试手动安装……"
+        loggreen "从wordpress官网下载包然后命名为latest-zh_CN.zip，新建目录/usr/share/wordpresstemp/，上传到此目录下即可"
+        exit 1
+    fi
+    loggreen "==============="
+    loggreen " 1.安装必要软件"
+    loggreen "==============="
+    sleep 1s
+    echo
+    wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+    wget https://rpms.remirepo.net/enterprise/remi-release-7.rpm
+    if [ -f "epel-release-latest-7.noarch.rpm" -a -f "remi-release-7.rpm" ]; then
+        loggreen "下载软件源成功"
+    else
+        logred "下载软件源失败，退出安装"
+        exit 1
+    fi
+    logcmd "rpm -Uvh remi-release-7.rpm epel-release-latest-7.noarch.rpm --force --nodeps"
+    #sed -i "0,/enabled=0/s//enabled=1/" /etc/yum.repos.d/epel.repo
+    yum -y install  unzip vim tcl expect curl socat
+    echo
+    echo
+    green "============"
+    green "2.安装PHP7.4"
+    green "============"
+    sleep 1
+    yum -y install php74 php74-php-gd php74-php-opcache php74-php-pdo php74-php-mbstring php74-php-cli php74-php-fpm php74-php-mysqlnd php74-php-xml
+    service php74-php-fpm start
+    chkconfig php74-php-fpm on
+    if [ `yum list installed | grep php74 | wc -l` -ne 0 ]; then
+        echo
+        green "【checked】 PHP7安装成功"
+        echo
+        echo
+        sleep 2
+        php_status=1
+    fi
+    green "==============="
+    green "  3.安装MySQL"
+    green "==============="
+    sleep 1
+    #wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
+    wget https://repo.mysql.com/mysql80-community-release-el7-3.noarch.rpm
+    rpm -ivh mysql80-community-release-el7-3.noarch.rpm --force --nodeps
+    yum -y install mysql-server
+    systemctl enable mysqld.service
+    systemctl start  mysqld.service
+    if [ `yum list installed | grep mysql-community | wc -l` -ne 0 ]; then
+        green "【checked】 MySQL安装成功"
+        echo
+        echo
+        sleep 2
+        mysql_status=1
+    fi
+    echo
+    echo
+    green "==============="
+    green "  4.配置MySQL"
+    green "==============="
+    sleep 2
+    originpasswd=`cat /var/log/mysqld.log | grep password | head -1 | rev  | cut -d ' ' -f 1 | rev`
+    mysqlpasswd=`mkpasswd -l 18 -d 2 -c 3 -C 4 -s 5 | sed $'s/[\'\/\;\"\:\.\?\&]//g'`
+cat > ~/.my.cnf <<EOT
+[mysql]
+user=root
+password="$originpasswd"
+EOT
+    mysql  --connect-expired-password  -e "alter user 'root'@'localhost' identified by '$mysqlpasswd';"
+    systemctl restart mysqld
+    sleep 5s
+cat > ~/.my.cnf <<EOT
+[mysql]
+user=root
+password="$mysqlpasswd"
+EOT
+    mysql  --connect-expired-password  -e "create database wordpress_db;"
+    echo
+    green "===================="
+    green " 5.配置php和php-fpm"
+    green "===================="
+    echo
+    echo
+    sleep 1
+    sed -i "s/upload_max_filesize = 2M/upload_max_filesize = 20M/;" /etc/opt/remi/php74/php.ini
+    sed -i "s/pm.start_servers = 5/pm.start_servers = 3/;s/pm.min_spare_servers = 5/pm.min_spare_servers = 3/;s/pm.max_spare_servers = 35/pm.max_spare_servers = 8/;" /etc/opt/remi/php74/php-fpm.d/www.conf
+    systemctl restart php74-php-fpm.service
+    systemctl restart nginx.service
+    green "===================="
+    green "  6.安装wordpress"
+    green "===================="
+    echo
+    echo
+    sleep 1
+    cd /usr/share/nginx/html
+    mv /usr/share/wordpresstemp/latest-zh_CN.zip ./
+    logcmd "unzip latest-zh_CN.zip"
+    mv wordpress/* ./
+    #cp wp-config-sample.php wp-config.php
+    logcmd "wget https://raw.githubusercontent.com/atrandys/trojan/master/wp-config.php"
+    green "===================="
+    green "  7.配置wordpress"
+    green "===================="
+    echo
+    echo
+    sleep 1
+    sed -i "s/database_name_here/wordpress_db/;s/username_here/root/;s?password_here?$mysqlpasswd?;" /usr/share/nginx/html/wp-config.php
+    #echo "define('FS_METHOD', "direct");" >> /usr/share/nginx/html/wp-config.php
+    chown -R apache:apache /usr/share/nginx/html/
+    #chmod 775 apache:apache /usr/share/nginx/html/ -Rf
+    chmod -R 775 /usr/share/nginx/html/wp-content
+    green "=========================================================================="
+    green " WordPress服务端配置已完成，请打开浏览器访问您的域名进行前台配置"
+    green " 数据库密码等信息参考文件：/usr/share/nginx/html/wp-config.php"
+    green "=========================================================================="
+    echo
+    if [ "$cert_failed" == "1" ]; then
+        loggreen "======nginx信息======"
+        logred "申请证书失败，请尝试手动申请证书."
+    fi    
+    loggreen "=================v2ray配置文件=================="
+    cat /usr/local/etc/v2ray/myconfig.json
+    loggreen "本次安装检测信息如下："
+    logcmd "ps -aux | grep -e nginx -e v2ray -e mysql -e php"
+}
+
 check_release(){
     loggreen "$(date +"%Y-%m-%d %H:%M:%S") ==== 检查系统版本"
     if [ "$RELEASE" == "centos" ]; then
@@ -66,41 +224,17 @@ check_release(){
         fi
         firewall_status=`firewall-cmd --state`
         if [ "$firewall_status" == "running" ]; then
-            loggreen "$(date +"%Y-%m-%d %H:%M:%S") - FireWalld状态非disabled,添加80/443到FireWalld rules."
-            firewall-cmd --zone=public --add-port=80/tcp --permanent
-            firewall-cmd --zone=public --add-port=443/tcp --permanent
-            firewall-cmd --reload
+            loggreen "$(date +"%Y-%m-%d %H:%M:%S") - FireWalld状态为running,关闭FireWalld."
+            #firewall-cmd --zone=public --add-port=80/tcp --permanent
+            #firewall-cmd --zone=public --add-port=443/tcp --permanent
+            #firewall-cmd --reload
+            logcmd "systemctl disable firewalld"
+            logcmd "systemctl stop firewalld"
         fi
         logcmd "rpm -Uvh http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm --force --nodeps"
         #loggreen "Prepare to install nginx."
         #yum install -y libtool perl-core zlib-devel gcc pcre* >/dev/null 2>&1
         logcmd "yum install -y epel-release"
-    elif [ "$RELEASE" == "ubuntu" ]; then
-        systemPackage="apt-get"
-        if  [ "$VERSION" == "14" ] ;then
-            logred "$(date +"%Y-%m-%d %H:%M:%S") - 暂不支持Ubuntu 14.\n== Install failed."
-            exit
-        fi
-        if  [ "$VERSION" == "12" ] ;then
-            logred "$(date +"%Y-%m-%d %H:%M:%S") - 暂不支持Ubuntu 12.\n== Install failed."
-            exit
-        fi
-        ufw_status=`systemctl status ufw | grep "Active: active"`
-        if [ -n "$ufw_status" ]; then
-            ufw allow 80/tcp
-            ufw allow 443/tcp
-            ufw reload
-        fi
-        apt-get update >/dev/null 2>&1
-    elif [ "$RELEASE" == "debian" ]; then
-        systemPackage="apt-get"
-        ufw_status=`systemctl status ufw | grep "Active: active"`
-        if [ -n "$ufw_status" ]; then
-            ufw allow 80/tcp
-            ufw allow 443/tcp
-            ufw reload
-        fi
-        apt-get update >/dev/null 2>&1
     else
         logred "$(date +"%Y-%m-%d %H:%M:%S") - 当前系统不被支持. \n== Install failed."
         exit
@@ -155,20 +289,7 @@ http {
 }
 EOF
 
-cat > /etc/nginx/conf.d/default.conf<<-EOF
- server {
-    listen       127.0.0.1:37212;
-    server_name  $your_domain;
-    root /usr/share/nginx/html;
-    index index.php index.html index.htm;
-}
- server {
-    listen       127.0.0.1:37213 http2;
-    server_name  $your_domain;
-    root /usr/share/nginx/html;
-    index index.php index.html index.htm;
-}
-    
+cat > /etc/nginx/conf.d/default.conf<<-EOF    
 server { 
     listen       0.0.0.0:80;
     server_name  $your_domain;
@@ -190,6 +311,40 @@ EOF
         cert_failed="1"
         logred "$(date +"%Y-%m-%d %H:%M:%S") - 申请证书失败，请尝试手动申请证书."
     fi
+cat > /etc/nginx/conf.d/default.conf<<-EOF
+server {
+    listen       127.0.0.1:80;
+    server_name  $your_domain;
+    root /usr/share/nginx/html;
+    index index.php index.html index.htm;
+    location ~ \.php$ {
+        fastcgi_pass 127.0.0.1:9000;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+        include fastcgi_params;
+    }
+    location / {
+        try_files \$uri \$uri/ /index.php?\$args;
+    }
+    
+}
+server {
+    listen       0.0.0.0:80;
+    server_name  $your_domain;
+    return 301 https://$your_domain\$request_uri;
+}
+ server {
+    listen       127.0.0.1:37212;
+    server_name  $your_domain;
+    root /usr/share/nginx/html;
+    index index.php index.html index.htm;
+}
+ server {
+    listen       127.0.0.1:37213 http2;
+    server_name  $your_domain;
+    root /usr/share/nginx/html;
+    index index.php index.html index.htm;
+}
+EOF
     install_v2ray
 }
 
@@ -306,8 +461,8 @@ EOF
     if [ -d "/usr/share/nginx/html/" ]; then
         cd /usr/share/nginx/html/ && rm -f ./*
         #wget https://github.com/atrandys/v2ray-ws-tls/raw/master/web.zip >/dev/null 2>&1
-        logcmd "wget https://github.com/atrandys/trojan/raw/master/fakesite.zip"
-        logcmd "unzip -o fakesite.zip"
+        #logcmd "wget https://github.com/atrandys/trojan/raw/master/fakesite.zip"
+        #logcmd "unzip -o fakesite.zip"
         #unzip web.zip >/dev/null 2>&1
     fi
     #systemctl stop v2ray
@@ -338,16 +493,7 @@ id：${v2uuid}
 }
 EOF
 
-    loggreen "== 安装完成."
-    if [ "$cert_failed" == "1" ]; then
-        loggreen "======nginx信息======"
-        logred "申请证书失败，请尝试手动申请证书."
-    fi    
-    loggreen "=================v2ray配置文件=================="
-    cat /usr/local/etc/v2ray/myconfig.json
-    loggreen "本次安装检测信息如下："
-    logcmd "ps -aux | grep -e nginx -e v2ray"
-    
+    loggreen "== v2ray安装完成."
 }
 
 check_domain(){
@@ -391,6 +537,10 @@ remove_v2ray(){
     rm -rf /etc/nginx
     rm -rf /usr/share/nginx/html/*
     rm -rf /root/.acme.sh/
+    yum remove -y php74 php74-php-gd  php74-php-pdo php74-php-opcache php74-php-mbstring php74-php-cli php74-php-fpm php74-php-mysqlnd mysql
+    rm -rf /var/lib/mysql	
+    rm -rf /usr/lib64/mysql
+    rm -rf /usr/share/mysql
     loggreen "nginx & v2ray has been deleted."
     
 }
