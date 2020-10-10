292a8993930e3694cebc76a65063cd25ccfe55b5
atrandys <37266675+atrandys@users.noreply.github.com>
2020-10-10 11:41:09 +0800

    Update vless_tcp_xtls_wp.sh

--- vless_tcp_xtls_wp.sh
+++ vless_tcp_xtls_wp.sh
@@ -59,9 +59,9 @@ install_wordpress(){
     iptables -P FORWARD DROP
     iptables -P OUTPUT ACCEPT
     service iptables save
-    green "====================================================================="
-    green "安全起见，iptables仅开启ssh,http,https端口，如需开放其他端口请自行放行"
-    green "====================================================================="
+    loggreen "====================================================================="
+    loggreen "安全起见，iptables仅开启ssh,http,https端口，如需开放其他端口请自行放行"
+    loggreen "====================================================================="
     sleep 1
     yum -y install  wget
     mkdir /usr/share/wordpresstemp
@@ -91,36 +91,36 @@ install_wordpress(){
     fi
     logcmd "rpm -Uvh remi-release-7.rpm epel-release-latest-7.noarch.rpm --force --nodeps"
     #sed -i "0,/enabled=0/s//enabled=1/" /etc/yum.repos.d/epel.repo
-    yum -y install  unzip vim tcl expect curl socat
+    yum -y install unzip vim tcl expect curl socat
     echo
     echo
-    green "============"
-    green "2.安装PHP7.4"
-    green "============"
+    loggreen "============"
+    loggreen "2.安装PHP7.4"
+    loggreen "============"
     sleep 1
-    yum -y install php74 php74-php-gd php74-php-opcache php74-php-pdo php74-php-mbstring php74-php-cli php74-php-fpm php74-php-mysqlnd php74-php-xml
+    logcmd "yum -y install php74 php74-php-gd php74-php-opcache php74-php-pdo php74-php-mbstring php74-php-cli php74-php-fpm php74-php-mysqlnd php74-php-xml"
     service php74-php-fpm start
     chkconfig php74-php-fpm on
     if [ `yum list installed | grep php74 | wc -l` -ne 0 ]; then
         echo
-        green "【checked】 PHP7安装成功"
+        loggreen "【checked】 PHP7安装成功"
         echo
         echo
-        sleep 2
+        sleep 2s
         php_status=1
     fi
-    green "==============="
-    green "  3.安装MySQL"
-    green "==============="
-    sleep 1
+    loggreen "==============="
+    loggreen "  3.安装MySQL"
+    loggreen "==============="
+    sleep 1s
     #wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
     wget https://repo.mysql.com/mysql80-community-release-el7-3.noarch.rpm
-    rpm -ivh mysql80-community-release-el7-3.noarch.rpm --force --nodeps
+    logcmd "rpm -ivh mysql80-community-release-el7-3.noarch.rpm --force --nodeps"
     yum -y install mysql-server
     systemctl enable mysqld.service
     systemctl start  mysqld.service
     if [ `yum list installed | grep mysql-community | wc -l` -ne 0 ]; then
-        green "【checked】 MySQL安装成功"
+        loggreen "【checked】 MySQL安装成功"
         echo
         echo
         sleep 2
@@ -128,9 +128,9 @@ install_wordpress(){
     fi
     echo
     echo
-    green "==============="
-    green "  4.配置MySQL"
-    green "==============="
+    loggreen "==============="
+    loggreen "  4.配置MySQL"
+    loggreen "==============="
     sleep 2
     originpasswd=`cat /var/log/mysqld.log | grep password | head -1 | rev  | cut -d ' ' -f 1 | rev`
     mysqlpasswd=`mkpasswd -l 18 -d 2 -c 3 -C 4 -s 5 | sed $'s/[\'\/\;\"\:\.\?\&]//g'`
@@ -149,31 +149,31 @@ password="$mysqlpasswd"
 EOT
     mysql  --connect-expired-password  -e "create database wordpress_db;"
     echo
-    green "===================="
-    green " 5.配置php和php-fpm"
-    green "===================="
+    loggreen "===================="
+    loggreen " 5.配置php和php-fpm"
+    loggreen "===================="
     echo
     echo
-    sleep 1
+    sleep 1s
     sed -i "s/upload_max_filesize = 2M/upload_max_filesize = 20M/;" /etc/opt/remi/php74/php.ini
     sed -i "s/pm.start_servers = 5/pm.start_servers = 3/;s/pm.min_spare_servers = 5/pm.min_spare_servers = 3/;s/pm.max_spare_servers = 35/pm.max_spare_servers = 8/;" /etc/opt/remi/php74/php-fpm.d/www.conf
     systemctl restart php74-php-fpm.service
     systemctl restart nginx.service
-    green "===================="
-    green "  6.安装wordpress"
-    green "===================="
+    loggreen "===================="
+    loggreen "  6.安装wordpress"
+    loggreen "===================="
     echo
     echo
-    sleep 1
+    sleep 1s
     cd /usr/share/nginx/html
     mv /usr/share/wordpresstemp/latest-zh_CN.zip ./
     logcmd "unzip latest-zh_CN.zip"
     mv wordpress/* ./
     #cp wp-config-sample.php wp-config.php
     logcmd "wget https://raw.githubusercontent.com/atrandys/trojan/master/wp-config.php"
-    green "===================="
-    green "  7.配置wordpress"
-    green "===================="
+    loggreen "===================="
+    loggreen "  7.配置wordpress"
+    loggreen "===================="
     echo
     echo
     sleep 1
@@ -182,10 +182,10 @@ EOT
     chown -R apache:apache /usr/share/nginx/html/
     #chmod 775 apache:apache /usr/share/nginx/html/ -Rf
     chmod -R 775 /usr/share/nginx/html/wp-content
-    green "=========================================================================="
-    green " WordPress服务端配置已完成，请打开浏览器访问您的域名进行前台配置"
-    green " 数据库密码等信息参考文件：/usr/share/nginx/html/wp-config.php"
-    green "=========================================================================="
+    loggreen "=========================================================================="
+    loggreen " WordPress服务端配置已完成，请打开浏览器访问您的域名进行前台配置"
+    loggreen " 数据库密码等信息参考文件：/usr/share/nginx/html/wp-config.php"
+    loggreen "=========================================================================="
     echo
     if [ "$cert_failed" == "1" ]; then
         loggreen "======nginx信息======"
@@ -346,6 +346,7 @@ server {
 }
 EOF
     install_v2ray
+    install_wordpress
 }
 
 install_v2ray(){ 
@@ -547,15 +548,15 @@ remove_v2ray(){
 
 function start_menu(){
     clear
-    loggreen " ====================================================="
-    loggreen " 描述：v2ray(vless) + tcp + xtls一键安装脚本"
-    loggreen " 系统：支持centos7/debian9+/ubuntu16.04+     "
+    loggreen " ======================================================"
+    loggreen " 描述：v2ray(vless) + tcp + xtls + wordpress一键安装脚本"
+    loggreen " 系统：支持centos7"
     loggreen " 作者：atrandys  www.atrandys.com"
-    loggreen " ====================================================="
+    loggreen " ======================================================"
     echo
-    loggreen " 1. 安装 vless + tcp + xtls"
+    loggreen " 1. 安装 vless + tcp + xtls + wordpress"
     loggreen " 2. 更新 v2ray"
-    logred " 3. 删除 v2ray"
+    logred " 3. 删除 v2ray + wordpress"
     logyellow " 0. Exit"
     echo
     read -p "输入数字:" num
